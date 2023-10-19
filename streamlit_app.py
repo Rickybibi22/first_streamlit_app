39cd18fff086466d2a04786808522a70a4f76bc7
Rickybibi22 <148269614+Rickybibi22@users.noreply.github.com>
2023-10-19 11:21:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,23 @@ streamlit.write('The user entered ', fruit_choice)
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized)
 
-streamlit.stop();
+#streamlit.stop();
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+
 streamlit.header("the fruit load list contains:")
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_data_rows = my_cur.fetchall()
+
+#add buttom
+if streamlit.button('Get Fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
